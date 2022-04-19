84ecbc32c8774111e2113cf6795656ae2901962c
Seungwoo Son <sson68x@gmail.com>
2022-04-18 23:07:51 -0700

    need to fix the issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+import Card from 'react-bootstrap/Card'
+// import Forms from './Forms';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: {},
+      cityName: 'City: ',
+      cityLat: 0,
+      cityLon: 0,
+      error: false,
+      errorMsg: ''
+    }
+  }
+
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+  }
+
+  handleCitySubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.location}&format=json`;
+      let cityData = await axios.get(url);
+      this.setState({
+        cityData: cityData.data[0],
+        cityName: cityData.data[0].display_name,
+        cityLat: cityData.data[0].cityLat,
+        cityLon: cityData.data[0].cityLon
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMsg: `An Error Occurred: ${error.response.status}`
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <form onSubmit={this.handleCitySubmit}>
+            <input
+              type="text"
+              name="city"
+              onInput={this.handleCityInput}
+              placeholder="Search for City"
+            />
+            <button type="submit">Explore!</button>
+          </form>
+        </div>
+        <Card style={{
+          width: '20em',
+          height: '25em',
+          textAlign: 'center',
+          backgroundColor: 'lightgreen',
+        }}>
+          <Card.Img variant='top'
+            src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.locationLat},${this.state.locationLon}&zoom=12`} />
+
+          <Card.Body style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            {
+              this.state.error ?
+                <p style={{ textAlign: 'center' }}>
+                  {this.state.errorMsg}
+                </p> :
+                <p style={{ textAlign: 'center' }}>
+                </p>
+            }
+            <Card.Title>{this.state.city}</Card.Title>
+            <Card.Text>Latitude: {this.state.cityLat}</Card.Text>
+            <Card.Text>Longitude: {this.state.cityLon}</Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    )
+  }
 }
-
 export default App;
